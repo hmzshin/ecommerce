d0d8b8c7565e39dee63d6e4645ef7a35a3b3ab61
hmzshin <hamzasahin@outlook.com>
2023-12-10 14:23:13 +0300

    create Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+
 function App() {
-  return <div>ecommerce project</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
