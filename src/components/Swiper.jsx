b1788c9fca38b6d050b0e55b83c71007c14fce00
hmzshin <hamzasahin@outlook.com>
2023-12-12 23:51:55 +0300

    simplifiy swiper component

--- src/components/Swiper.jsx
+++ src/components/Swiper.jsx
@@ -3,28 +3,22 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
-import "swiper/css/thumbs";
-import "swiper/css/free-mode";
 
 export default ({ slides, config }) => {
   return (
-    <Swiper
-      modules={config.modules}
-      spaceBetween={config.spaceBetween}
-      slidesPerView={1}
-      navigation
-      pagination={{ clickable: true }}
-      speed={config.speed}
-      direction={"horizontal"}
-      mousewheel={{
-        releaseOnEdges: true,
-        thresholdDelta: 1,
-      }}
-      className={config.className}
-    >
-      {slides.map((slide, i) => (
-        <SwiperSlide key={i}>{slide}</SwiperSlide>
-      ))}
-    </Swiper>
+    <>
+      <Swiper
+        spaceBetween={config.spaceBetween}
+        navigation={true}
+        speed={config.speed}
+        modules={config.modules}
+        className={config.className}
+        mousewheel={{ releaseOnEdges: true, invert: true }}
+      >
+        {slides.map((slide, i) => (
+          <SwiperSlide key={i}>{slide}</SwiperSlide>
+        ))}
+      </Swiper>
+    </>
   );
 };
