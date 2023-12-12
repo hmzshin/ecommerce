fd7dd5941dda498f60c0d413efa2dd31a932a12d
hmzshin <hamzasahin@outlook.com>
2023-12-12 23:53:37 +0300

    create productPage layout  and ProductDetail component

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,18 +1,22 @@
 import BestSellerProductCard from "../components/BestsellerProductCard-product";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import ProductDetailCard from "../components/ProductDetailCard";
+import ProductDetail from "../components/ProductDetail";
 
 import { productRelatedBestsellers } from "../data";
+import Clients from "../components/Clients";
 
 const product = {};
 const ProductPage = () => {
   return (
     <>
       <Header />
-      <ProductDetailCard product={product} />
-      <section id="bestsellers">
-        <div className=" flex flex-col items-center gap-20 px-[15%] py-20">
+      <ProductDetail product={product} />
+      <section id="bestsellers" className="px-[15%] py-20 bg-neutral-50 ">
+        <p className=" text-slate-800 text-2xl font-bold font-['Montserrat'] tracking-tight">
+          BESTSELLER PRODUCTS
+        </p>
+        <div className=" flex flex-col items-center gap-20 pt-20">
           <div className=" flex flex-wrap gap-10 justify-around">
             {productRelatedBestsellers.map((bsProduct) => (
               <BestSellerProductCard bsProduct={bsProduct} />
@@ -20,7 +24,8 @@ const ProductPage = () => {
           </div>
         </div>
       </section>
-      <Footer />
+      <Clients bgColor={"bg-neutral-50"} />
+      <Footer bgColor={"bg-white"} />
     </>
   );
 };
