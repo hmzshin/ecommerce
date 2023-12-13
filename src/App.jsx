ccdeb2158346d08bccac41998190c9300a084aa6
hmzshin <hamzasahin@outlook.com>
2023-12-13 17:49:52 +0300

    create aboutPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import ShopPage from "./pages/ShopPage.jsx";
 import ProductPage from "./pages/ProductPage.jsx";
+import AboutPage from "./pages/AboutPage.jsx";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" exact element={<HomePage />} />
         <Route path="/shop" exact element={<ShopPage />} />
         <Route path="/product" exact element={<ProductPage />} />
+        <Route path="/about" exact element={<AboutPage />} />
       </Routes>
     </>
   );
---
+++ src/components/HeaderInnerPages.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Icon } from "@iconify/react";
+
+const HeaderInnerPages = () => {
+  return (
+    <section id="header">
+      <div className="w-full h-14 px-[15%] my-5 flex gap-40 items-center">
+        <h1 className="text-slate-800 text-2xl font-bold font-['Montserrat'] leading-loose tracking-tight">
+          Bandage
+        </h1>
+        <div className="flex w-full justify-between items-center">
+          <nav>
+            <ul className="flex gap-10 ">
+              {[
+                { path: "/", text: "Home" },
+                { path: "/shop", text: "Shop" },
+                { path: "/about", text: "About" },
+                { path: "/contact", text: "Contact" },
+                { path: "", text: "Pages" },
+              ].map((link, i) => (
+                <Link
+                  to={link.path}
+                  className="text-neutral-500 text-base font-bold font-['Montserrat'] leading-normal tracking-tight"
+                >
+                  {link.text}
+                </Link>
+              ))}
+            </ul>
+          </nav>
+          <div className="flex gap-20 items-center text-center text-sky-500 font-bold font-['Montserrat'] tracking-tight">
+            <p>Login</p>
+
+            <div className="blueBtn">
+              Become a member
+              <Icon icon="formkit:arrowright" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeaderInnerPages;
