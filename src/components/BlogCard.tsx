1c797e35a5da61a23cc2d758f82fa4c4488262eb
hmzshin <hamzasahin@outlook.com>
2023-12-15 11:51:25 +0300

    fix: add "sale"mark to blog card top-left corner

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -4,7 +4,10 @@ import arrowRight from "../assets/blog/arrowIcon.svg";
 
 const BlogCard = ({ content }: any) => {
   return (
-    <div className="flex">
+    <div className="flex relative">
+      <div className="w-14 h-6 px-3 py-1 flex items-center justify-center bg-red-500 rounded-[3px] shadow text-white text-base font-bold font-['Montserrat'] tracking-tight absolute top-6 left-6">
+        Sale
+      </div>
       <img src={content.img} className="w-64" />
       <div className="w-96 p-7 flex-col justify-start items-start gap-5 flex">
         <div className="flex justify-between items-center self-stretch">
@@ -12,7 +15,7 @@ const BlogCard = ({ content }: any) => {
             {content.category}
           </p>
 
-          <div className="w-14 h-7 p-1 bg-slate-800 rounded-3xl flex justify-between items-center ">
+          <div className="w-14 h-7 py-1 px-2 gap-2 bg-slate-800 rounded-3xl flex  items-center justify-center">
             <img src={start} />
             <p className="text-white text-sm font-normal font-['Montserrat'] tracking-[0.2px]">
               {content.overview}
@@ -51,7 +54,7 @@ const BlogCard = ({ content }: any) => {
           ))}
         </div>
 
-        <p className="px-4 py-4 rounded-[37px] border border-sky-500 text-sky-500 text-lg font-bold font-['Montserrat'] leading-normal tracking-[0.2px] flex gap-2 whitespace-nowrap ">
+        <p className="px-6 py-4 rounded-[37px] border border-sky-500 text-sky-500 text-lg font-bold font-['Montserrat']  tracking-tight flex gap-2 whitespace-nowrap ">
           Learn More <img src={arrowRight} />
         </p>
       </div>
