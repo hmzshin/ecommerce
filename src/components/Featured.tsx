251d82b8b483f7ed5fee88d21a494ab152f418f3
hmzshin <hamzasahin@outlook.com>
2023-12-17 16:46:23 +0300

    refactor:  Rename component to 'Featured' and apply responsive design for mobile optimization

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -20,12 +20,12 @@ const features = [
   },
 ];
 
-const Features = () => {
+const Featured = () => {
   return (
     <section id="features">
-      <div className="flex flex-col gap-10 px-[15%] py-20">
-        <section>
-          <p className="text-center text-neutral-500 text-2xl font-normal font-['Montserrat'] leading-[30px] tracking-[0.2px]">
+      <div className="flex flex-col gap-10  lg:px-[5%] 2xl:px-[15%] py-20">
+        <section className="flex flex-col gap-5">
+          <p className="text-center text-neutral-500 text-2xl font-normal font-['Montserrat'] tracking-[0.2px]">
             Featured Products
           </p>
           <p className="text-center text-slate-800 text-3xl font-bold font-['Montserrat'] leading-10 tracking-[0.1px]">
@@ -35,10 +35,10 @@ const Features = () => {
             Problems trying to resolve the conflict between{" "}
           </p>
         </section>
-        <section className="flex justify-between ">
+        <section className="flex flex-wrap  justify-center gap-10 ">
           {features.map((feature) => (
             <div className="px-10 py-9 flex-col justify-start items-center gap-5 inline-flex">
-              <img src={feature.img} />
+              <img src={feature.img} className="w-32" />
               <p className="text-center text-slate-800 text-3xl font-bold font-['Montserrat'] leading-loose tracking-[0.2px]">
                 {feature.header}
               </p>
@@ -53,4 +53,4 @@ const Features = () => {
   );
 };
 
-export default Features;
+export default Featured;
