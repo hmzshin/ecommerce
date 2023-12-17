dbab5b5180ccfa61cb75647f95798b5c2c466b4d
hmzshin <hamzasahin@outlook.com>
2023-12-17 16:47:39 +0300

    style:  Apply responsive design  to Blog component for mobile optimization

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -3,7 +3,7 @@ import { contents } from "../data";
 const Blog = () => {
   return (
     <section id="blog">
-      <div className="flex flex-col items-center px-[15%] py-20 gap-20">
+      <div className="flex flex-col items-center lg:px-[5%] xl:px-[10%] 2xl:px-[15%] py-20 gap-20">
         <div className="w-96 h-24 flex-col justify-start items-center gap-5 inline-flex">
           <p className="text-center text-sky-500 font-bold font-['Montserrat'] leading-normal tracking-[0.2px]">
             Practice Advice
@@ -12,7 +12,7 @@ const Blog = () => {
             Featured Posts
           </p>
         </div>
-        <div className="flex justify-between w-full">
+        <div className="flex flex-wrap items-center gap-20 md:gap-20 xl:flex-nowrap justify-center w-full">
           {contents.map((content, i) => (
             <BlogCard key={i} content={content} />
           ))}
