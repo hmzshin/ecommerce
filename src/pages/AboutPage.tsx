6b51c53a80e9275398dd38d2ec4e67db57af8516
hmzshin <hamzasahin@outlook.com>
2023-12-18 10:55:20 +0300

    style: Implement responsive design in About Page  for improved mobile navigation and readability

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -13,42 +13,45 @@ const AboutPage = () => {
   return (
     <>
       <Header />
-      <section id="hero" className="px-[15%] flex pt-10">
-        <div className="w-1/2 flex flex-col gap-10 py-40">
-          <p className="text-slate-800 text-lg font-bold font-['Montserrat'] tracking-tight">
+      <section
+        id="hero"
+        className="flex flex-wrap justify-center px-[11%] pt-10"
+      >
+        <div className=" flex flex-col items-center gap-10 py-20 lg:items-start lg:w-1/2 lg:py-40   ">
+          <p className="hidden text-slate-800 text-lg font-bold font-['Montserrat'] tracking-tight lg:block">
             ABOUT COMPANY
           </p>
-          <p className=" text-slate-800 text-[58px] font-bold font-['Montserrat'] leading-[80px] tracking-tight">
+          <p className=" text-slate-800 text-5xl text-center font-bold font-['Montserrat'] tracking-tight lg:text-[58px]">
             ABOUT US
           </p>
-          <p className="w-96 text-neutral-500 text-xl font-normal font-['Montserrat'] tracking-tight">
+          <p className="max-w-sm text-neutral-500 text-xl font-normal font-['Montserrat'] tracking-tight text-center lg:text-left">
             We know how large objects will act, but things on a small scale
           </p>
           <p className="blueBtn w-56 ">Get Quote Now</p>
         </div>
-        <div className="w-1/2 ">
+        <div className="lg:w-1/2 max-h-[650px]">
           <img
             src={happyWoman}
             className="w-full h-full object-cover object center"
           />
         </div>
       </section>
-      <section className="flex items-center justify-between px-[17%] py-20">
+      <section className="flex flex-wrap gap-10 items-center justify-around px-[12% py-32  lg:flex-nowrap lg:px-[12%]">
         <div>
-          <p className="text-red-500 text-base font-normal font-['Montserrat'] tracking-tight">
+          <p className="text-red-500 text-base font-normal font-['Montserrat'] tracking-tight text-center lg:text-left ">
             Problems trying
           </p>
-          <p className="max-w-lg text-slate-800 text-3xl font-bold font-['Montserrat']  tracking-tight pt-10">
+          <p className="max-w-xl text-slate-800 text-3xl text-center font-bold font-['Montserrat']  tracking-tight pt-10 lg:text-left">
             Met minim Mollie non desert Alamo est sit cliquey dolor do met sent.
           </p>
         </div>
-        <p className="max-w-xl text-neutral-500 text-base font-normal font-['Montserrat'] tracking-tight">
+        <p className="max-w-sm text-neutral-500 text-base text-center font-normal font-['Montserrat'] tracking-tight lg:text-left xl:max-w-xl">
           Problems trying to resolve the conflict between the two major realms
           of Classical physics: Newtonian mechanics{" "}
         </p>
       </section>
 
-      <section className="flex items-center justify-between px-[15%] py-20 ">
+      <section className="flex flex-wrap items-center justify-center md:justify-around px-[15%] py-20  gap-20">
         {[
           { text1: "15K", text2: "Happy Customers" },
           { text1: "150K", text2: "Monthly Visitors" },
@@ -67,7 +70,11 @@ const AboutPage = () => {
       </section>
 
       <section className="px-[15%] py-20 relative ">
-        <div className=" w-24 h-24 absolute top-[calc(50%-48px)] left-[calc(50%-48px)] bg-sky-500 rounded-full p-5 text-white hover:scale-105 transition-all">
+        <div
+          className="w-16 h-16 md:w-24 md:h-24 absolute
+        top-[calc(50%-32px)] left-[calc(50%-32px)] 
+        md:top-[calc(50%-48px)] md:left-[calc(50%-48px)] bg-sky-500 rounded-full p-5 text-white hover:scale-105 transition-all"
+        >
           <Icon icon="ph:play-fill" className="w-full h-full" />
         </div>
         <img
@@ -78,10 +85,10 @@ const AboutPage = () => {
 
       <section
         id="team"
-        className="px-[15%] py-28 flex flex-col items-center gap-20"
+        className="px-[12%] py-28 flex flex-col items-center gap-20"
       >
         <div className="flex flex-col items-center gap-10">
-          <h3 className="text-slate-800 text-5xl font-bold font-['Montserrat'] tracking-tight">
+          <h3 className="text-slate-800 text-5xl text-center font-bold font-['Montserrat'] tracking-tight">
             Meet Our Team
           </h3>
           <p className="max-w-lg text-center text-neutral-500 text-base font-normal font-['Montserrat']  tracking-tight">
@@ -89,7 +96,7 @@ const AboutPage = () => {
             of Classical physics: Newtonian mechanics{" "}
           </p>
         </div>
-        <div className="flex items-center justify-around self-stretch">
+        <div className="flex flex-wrap gap-20 items-center justify-around self-stretch">
           {[
             { img: member1, username: "Username", profession: "engineer" },
             { img: member1, username: "Username", profession: "engineer" },
@@ -102,7 +109,7 @@ const AboutPage = () => {
 
       <section id="clients" className="bg-neutral-50 py-32">
         <div className="px-[15%] flex flex-col items-center gap-10 pb-10">
-          <h3 className="text-slate-800 text-[40px] font-bold font-['Montserrat'] leading-[50px] tracking-tight">
+          <h3 className="text-slate-800 text-[40px] text-center font-bold font-['Montserrat'] leading-[50px] tracking-tight">
             Big Companies Are Here
           </h3>
           <p className="max-w-xl text-center text-neutral-500 text-base font-normal font-['Montserrat']  tracking-tight">
@@ -113,18 +120,18 @@ const AboutPage = () => {
         <Clients />
       </section>
 
-      <section className="flex h-[70vh]">
+      <section className="flex flex-wrap justify-center md:h-[70vh]">
         <div
-          className="w-[60%] h-full bg-sky-600 px-[15%] flex flex-col items-start
-         justify-center gap-10"
+          className="flex flex-col items-center justify-center  gap-10 px-[15%] py-20 bg-sky-600  md:items-start
+         md:w-[60%] md:h-full"
         >
-          <p className="text-white text-lg font-bold font-['Montserrat'] tracking-tight">
+          <p className="text-white text-lg  font-bold font-['Montserrat'] tracking-tight">
             Work With Us
           </p>
-          <p className="text-white text-5xl font-bold font-['Montserrat']tracking-tight">
+          <p className="text-white text-5xl text-center md:text-start font-bold font-['Montserrat']tracking-tight">
             Now Let’s grow Yours
           </p>
-          <p className="max-w-md text-white text-base font-normal font-['Montserrat'] tracking-tight">
+          <p className="max-w-md text-white text-base text-center md:text-start font-normal font-['Montserrat'] tracking-tight">
             The gradual accumulation of information about atomic and small-scale
             behavior during the first quarter of the 20th{" "}
           </p>
@@ -132,7 +139,7 @@ const AboutPage = () => {
             Button
           </div>
         </div>
-        <div className="w-[40%] border-[2px] border-sky-600">
+        <div className="hidden w-[40%] max-h-full border-[2px] border-sky-600 md:block  ">
           <img
             src={workWithUs}
             className=" w-full h-full object-cover object-top"
