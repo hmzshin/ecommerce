72d39bf64de85ff93a1ef7372d78c7c90bff091a
hmzshin <hamzasahin@outlook.com>
2023-12-21 22:24:59 +0300

    feature : Create combine reducer and import reducers to Redux store

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,11 +3,17 @@ import { configureStore } from "@reduxjs/toolkit";
 import { globalSlice } from "./slices/globalSlice";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { userSlice } from "./slices/userSlice";
+import { productSlice } from "./slices/productSlice";
+import { shoppingCardSlice } from "./slices/shoppingCardSlice";
+import { storeSlice } from "./slices/storeSlice";
 
 export const store = configureStore({
   reducer: {
     global: globalSlice.reducer,
     user: userSlice.reducer,
+    product: productSlice.reducer,
+    shoppingCard: shoppingCardSlice.reducer,
+    store: storeSlice.reducer,
   },
 });
 
