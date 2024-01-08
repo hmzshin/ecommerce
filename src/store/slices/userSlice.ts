4a15db635bcf30efee9342fcacdc71569fb05d78
hmzshin <hamzasahin@outlook.com>
2024-01-08 23:38:34 +0300

    fix:Fix error occurring while loading saved addresses in the code.

--- src/store/slices/userSlice.ts
+++ src/store/slices/userSlice.ts
@@ -1,6 +1,7 @@
 import { PayloadAction, createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { AxiosResponse } from "axios";
 import { axiosInstance } from "../../api/axiosInstance";
+import { RootState } from "../store";
 
 interface Payload {
   email: string;
@@ -11,12 +12,14 @@ interface UserData {
   name: string;
   email: string;
   role_id: string;
+  token: string;
 }
 
 const initialState: UserData = {
   name: "",
   email: "",
   role_id: "",
+  token: "",
 };
 
 export const sendLoginInfo = createAsyncThunk(
@@ -41,18 +44,25 @@ export const userSlice = createSlice({
         email: action.payload.email,
         name: action.payload.name,
         role_id: action.payload.role_id,
+        token: action.payload.token,
       };
     },
   },
 
   extraReducers(builder) {
     builder.addCase(sendLoginInfo.fulfilled, (state, action: any) => {
-      state.email = action.payload.email;
-      state.name = action.payload.name;
-      state.role_id = action.payload.role_id;
+      return {
+        ...state,
+        email: action.payload.email,
+        name: action.payload.name,
+        role_id: action.payload.role_id,
+        token: action.payload.token,
+      };
     });
   },
 });
+
+export const selectToken = (state: RootState) => state.user.token;
 
 export default userSlice.reducer;
 export const { setUser } = userSlice.actions;
